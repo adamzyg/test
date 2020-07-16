9e1db70de3d36c134e684f4de51828e91f70f2f2
zhengyagang <zhengyagang@cigtech.com>
2020-07-16 15:58:41 +0800

    update

--- test_static_var.c
+++ test_static_var.c
@@ -2,9 +2,13 @@
 
 int func()
 {
+    char test[10] = {0};
+    static char *p = "first";
     static int a = 5;
     a++;
-    printf("current static int a = %d\n", a);
+    printf("current static int a = %d, statis char *p = %s\n", a, p);
+    sprintf(test, "%d", a);
+    p = test;
     return 0;
 }
 
